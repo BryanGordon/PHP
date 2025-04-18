87a80ef0b28eafdb64e7d9eeec86c89fe2fbeed0
Bryan Gordon <bryanbge92@gmail.com>
2025-04-17 22:56:37 -0500

    Create index file

---
+++ remembering-php/index.php
@@ -0,0 +1,21 @@
+<?php 
+  $name = 'Bryan';
+
+  $output = "Hola $name";
+
+?>
+
+<h1>
+  <?= $output ?>
+</h1>
+
+<style>
+  :root {
+    color-scheme: light dark;
+  }
+
+  body {
+    display: grid;
+    place-content: center;
+  }
+</style>
