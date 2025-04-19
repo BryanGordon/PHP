66fa144c91d95587e5f02136397f1ee8179c213c
Bryan Gordon <bryanbge92@gmail.com>
2025-04-19 01:31:57 -0500

    Create index

---
+++ last-movie-mcu/index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<main>
+  <h2>La proxima pelicula de marvel</h2>
+</main>
+
+<style>
+  :root {
+    color-scheme: light dark;
+  }
+
+  body {
+    display: grid;
+    place-content: center;
+  }
+</style>
