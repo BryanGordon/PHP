dc8f18cf93e4c510e2bcc3501cfed866873e474e
Bryan Gordon <bryanbge92@gmail.com>
2025-04-21 22:22:53 -0500

    Create api call

--- last-movie-mcu/index.php
+++ last-movie-mcu/index.php
@@ -1,9 +1,39 @@
 <?php
+const API_URL = "https://whenisthenextmcufilm.com/api";
+# Inicializar una nueva sesion  de cURL; ch = cURL handle
+$ch = curl_init(API_URL);
 
+#Evitar que se mientre la informacion de la api en pantalla
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+# Ejecutamos la peticion y guardamos los resultados
+$result = curl_exec($ch);
+# Otra forma de hacer un llamado a la api (solo GET)
+# $result = file_get_contents(API_URL);
+$data = json_decode($result, true);
+
+curl_close($ch);
+
+var_dump($data);
 ?>
 
+<head>
+  <meta charset="UTF-8" />
+  <title>Peliculas MCU</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css">
+</head>
+
 <main>
-  <h2>La proxima pelicula de marvel</h2>
+  <section>
+    <img src="<?=$data["poster_url"] ?>" width="300" alt="Poster de la pelicula <?= $data["title"] ?>" style="border-radius: 16px;">
+  </section>
+
+  <hgroup>
+    <h3><?= $data["title"]?></h3>
+  </hgroup>
 </main>
 
 <style>
