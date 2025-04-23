f1262ca7dbaafe4336dcef8cd869edf556b9f157
Bryan Gordon <bryanbge92@gmail.com>
2025-04-23 17:35:47 -0500

    Update variables declarations

--- last-movie-mcu/templates/main.php
+++ last-movie-mcu/templates/main.php
@@ -1,12 +1,12 @@
 <main>
   <section>
-    <img src="<?=$data["poster_url"]; ?>" width="300" alt="Poster de la pelicula <?= $data["title"]; ?>" style="border-radius: 16px;">
+    <img src="<?=$poster_url; ?>" width="300" alt="Poster de la pelicula <?= $title; ?>" style="border-radius: 16px;">
   </section>
 
   <hgroup>
-    <h3><?= $data["title"]; ?></h3>
-    <span style="font-weight: bold;"><?= $untilMessage ?> </span>
-    <p>Fecha de estreno: <?= $data["release_date"]; ?></p>
-    <p>La siguiente es: <?= $data["following_production"]["title"]; ?></p>
+    <h3><?= $title ?></h3>
+    <span style="font-weight: bold;"><?= $until_message ?> </span>
+    <p>Fecha de estreno: <?= $release_date ?></p>
+    <p>La siguiente es: <?= $following_production; ?></p>
   </hgroup>
 </main>
