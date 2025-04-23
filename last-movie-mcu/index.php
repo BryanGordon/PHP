03ef64ae8f499ab9375d741636299f1a36b97d9f
Bryan Gordon <bryanbge92@gmail.com>
2025-04-23 17:35:14 -0500

    Restructure index file

--- last-movie-mcu/index.php
+++ last-movie-mcu/index.php
@@ -20,12 +20,14 @@ curl_close($ch);
 */
 require_once 'consts.php';
 require_once 'functions.php';
+require_once './classes/NextMovie.php';
 
-$data = get_data(API_URL);
-$untilMessage = get_until_message($data["days_until"])
-
+$next_movie = NextMovie::fetch_and_create_movie(API_URL);
+$next_movie_data = $next_movie->get_data();
 ?>
 
-<?php render_template('head', $data);  ?>
-<?php require 'sections/main.php'?>
+<?php render_template('head', ["title" => $next_movie_data["title"]]);  ?>
+<?php render_template('main', array_merge($next_movie_data, [
+  "until_message" => $next_movie->get_until_message($next_movie_data["days_until"])
+])) ?>
 <?php render_template('styles'); ?>
